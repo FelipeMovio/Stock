cf49843fa2af82bb42daf41626ea530c22244ae9
FelipeMovio <felipelsmovio@gmail.com>
2025-10-05 21:03:30 -0300

    dependecias

---
+++ src/test/java/com/felipemovio/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemovio.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
