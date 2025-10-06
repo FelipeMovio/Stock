671b798cb692183c5f80c9f67f311f6201fc20d5
FelipeMovio <felipelsmovio@gmail.com>
2025-10-06 18:33:41 -0300

    create repository

---
+++ src/main/java/com/felipemovio/stock/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.felipemovio.stock.repository;
+
+import com.felipemovio.stock.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
