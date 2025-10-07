d911f9145bb9842a2a134748c0173feb11b8fb29
FelipeMovio <felipelsmovio@gmail.com>
2025-10-07 20:08:54 -0300

    ...

--- src/main/java/com/felipemovio/stock/controller/PorductController.java
+++ src/main/java/com/felipemovio/stock/controller/PorductController.java
@@ -1,4 +1,9 @@
 package com.felipemovio.stock.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Product")
 public class PorductController {
 }
