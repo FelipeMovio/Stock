fbe45667d508431ccb7e2d840c6822f647cbe509
FelipeMovio <felipelsmovio@gmail.com>
2026-05-13 13:38:18 -0300

    update

--- src/main/java/com/felipemovio/stock/controller/FilterPoductController.java
+++ src/main/java/com/felipemovio/stock/controller/FilterPoductController.java
@@ -1,10 +1,12 @@
 package com.felipemovio.stock.controller;
 
 import com.felipemovio.stock.dto.ProductResponseMiniDTO;
+import com.felipemovio.stock.model.Category;
 import com.felipemovio.stock.service.FilterProductsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +21,17 @@ public class FilterPoductController {
 
     @GetMapping("/melhores")
     public ResponseEntity <List<ProductResponseMiniDTO>> melhores(){
-        return ResponseEntity.ok(filterProductsService.melhores());
+        return ResponseEntity.ok(filterProductsService.top5());
     }
 
     @GetMapping("/melhores/baratos")
     public ResponseEntity <List<ProductResponseMiniDTO>> melhoresBaratos(){
         return ResponseEntity.ok(filterProductsService.melhoresAndBaratos());
+    }
+
+    @GetMapping("/busca/categoria")
+    public ResponseEntity<List<ProductResponseMiniDTO>> porCategoria(@RequestBody Category category){
+        return ResponseEntity.ok(filterProductsService.buscaPorCategoria(category));
     }
 
 
