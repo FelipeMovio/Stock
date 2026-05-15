77d957975af0353da09ad31a3f769072404fd027
FelipeMovio <felipelsmovio@gmail.com>
2026-05-15 10:51:10 -0300

    mediaController

--- src/main/java/com/felipemovio/stock/controller/MediaController.java
+++ src/main/java/com/felipemovio/stock/controller/MediaController.java
@@ -1,8 +1,11 @@
 package com.felipemovio.stock.controller;
 
+import com.felipemovio.stock.dto.ProductResponseStatsDTO;
 import com.felipemovio.stock.service.FilterProductsService;
 import com.felipemovio.stock.service.MediaProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MediaController {
 
     private final MediaProductService mediaProductService;
+
+    @GetMapping
+    public ResponseEntity<ProductResponseStatsDTO> medias(){
+        return ResponseEntity.ok(mediaProductService.media());
+    }
 
 }
